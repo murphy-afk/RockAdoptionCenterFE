7d5bb0b4db7a3869c5ac21fb90b6052738380cef
murphy-afk <bardarenoemi@gmail.com>
2026-05-19 18:29:43 +0200

    rock api call test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
+import axios from 'axios'
 
 function App() {
+
+  axios.get('http://127.0.0.1:8000/api/rocks')
+  .then(response => {
+    console.log(response.data);
+  })
 
 
   return (
     <>
       <h1>Rock Adoption Center</h1>
+      
     </>
   )
 }
